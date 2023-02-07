286c70254d4e72f3d26197f0c35e0422bfa59827
danawelsh <111257868+danawelsh@users.noreply.github.com>
2023-02-07 14:41:11 -0500

    redirected pathway for readme generator

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const inquirer = require('inquirer');
 const generateMarkdown = require("./utils/generateMarkdown")
 
 const questions = [
     {
         type: 'input',
-        message: 'What is your github repository?',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is the name of your GitHub repo?',
         name: 'repo',
     },
     {
@@ -17,32 +23,33 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What is a detailed description of your application?',
+        message: 'Provide a detailed description of your application, including what a user should expect to see and buttons or items they can utilize.',
         name: 'description',
     },
     {
         type: 'input',
-        message: 'What is your installation process?',
+        message: 'If applicable, provide instructions on how to install your application.',
         name: 'installation',
     },
     {
         type: 'input',
-        message: 'How does one use your application?',
+        message: 'Provide instructions and examples of your application in use.',
         name: 'usage',
     },
     {
         type: 'input',
-        message: 'Who is receiving credit for this application?',
+        message: 'If applicable, provide guidelines on how others may contribute to your application.',
         name: 'contributing',
     },
     {
-        type: 'input',
+        type: 'list',
         message: 'What is your application license?',
         name: 'license',
+        choices: ['MIT', 'ISC', 'Mozilla', 'None']
     },
     {
         type: 'input',
-        message: 'How to run the tests to your application?',
+        message: 'If applicable, provide a list of tests written for your application with examples.',
         name: 'tests',
     },
     {
@@ -58,21 +65,29 @@ const questions = [
 ];
 
 const handleResponse = (response) => {
-    console.log(response);
     const template = generateMarkdown(response)
-    writeToFile('README.md', template);
+    console.log(template);
+    writeToFile('./dist/README.md', template);
 }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Success! Your README.md file has been generated!")
+    });
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
       .prompt(questions)
-      .then(handleResponse);
+      .then(handleResponse)
+    .catch((err) => {
+        console.log(err);
+    })
 }
 
 // Function call to initialize app
